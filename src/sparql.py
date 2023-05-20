186b3958fe2c00132a35beeb3d69a77aeee27549
Nils Remigius <nils.remigius@study.hs-duesseldorf.de>
2023-05-20 21:34:01 +0200

    refactored queries, fetch multiple movies at once instead of just one
    - export of movie db with open data

--- src/sparql.py
+++ src/sparql.py
@@ -3,40 +3,41 @@ import requests
 import pandas as pd
 import pymongo
 
+# How many movies should be processed at once
+FETCH_AMOUNT = 25
 file = 'movies_metadata.csv'
 df = pd.read_csv(file)
 df.reset_index(inplace=True)
-data_dict = df.to_dict("records")
-
+movieDataDict = df.to_dict("records")
 client = pymongo.MongoClient()
 db = client['moviedb']
 collection = db['movie']
-
+ids = list(map(lambda doc: doc['imdb_id'], movieDataDict))
+idCount = len(ids)
 select = ["?film", "?filmLabel", "?id", "?director", "?directorLabel", "?castMember", "?castMemberLabel", "?voiceActor",
-          "?voiceActorLabel",
-          "?producer", "?producerLabel", "?composer", "?composerLabel", "?mainSubject", "?mainSubjectLabel",
-          "?follows", "?followsLabel", "?followedBy", "?followedByLabel", "?publicationDate", "?cost"]
-for movie in data_dict:
+          "?voiceActorLabel", "?producer", "?producerLabel", "?composer", "?composerLabel", "?mainSubject",
+          "?mainSubjectLabel", "?follows", "?followsLabel", "?followedBy", "?followedByLabel", "?publicationDate",
+          "?cost"]
+fetchedElements = 0
+
+while fetchedElements < idCount:
+    currentIds = ids[fetchedElements:fetchedElements + FETCH_AMOUNT]
     select_query = SPARQLSelectQuery(distinct=True)
     # SELECT
     select_query.add_variables(select)
     # Create a graph pattern
     pattern = SPARQLGraphPattern()
-    # Add a couple of triples to the pattern
+
     # WHERE
     pattern.add_triples(
         triples=[
-             Triple(subject="?film", predicate="wdt:P3302", object="?id"),
-            #Triple(subject="?film", predicate="wdt:P31/wdt:P279*", object="wd:Q11424"),
-            #Triple(subject="?film", predicate="rdfs:label", object="?filmLabel"),
+            Triple(subject="?film", predicate="wdt:P345", object="?id"),
             Triple(subject="SERVICE", predicate="wikibase:label", object="{ bd:serviceParam wikibase:language 'en'. }")
         ]
     )
 
     # Filter
-    #pattern.add_filter(filter=Filter(expression="LANG(?filmLabel)='en'"))
-    #pattern.add_filter(filter=Filter(expression="REGEX(?filmLabel,'^{0}$')".format(movie['title'])))
-    pattern.add_filter(filter=Filter(expression="?id='{0}'".format(movie['id'])))
+    pattern.add_filter(filter=Filter(expression="?id IN({0})".format(', '.join(f'"{_id}"' for _id in currentIds))))
 
     for option in [
         {'predicate': "wdt:P57", 'object': "?director"},
@@ -59,34 +60,53 @@ for movie in data_dict:
         pattern.add_nested_graph_pattern(optionalPattern)
 
     select_query.set_where_pattern(pattern)
-
-    # Let's print this graph pattern
-    print(select_query.get_text())
-
     r = requests.get('https://query.wikidata.org/sparql', params={'query': select_query.get_text()},
-                     headers={'accept': 'application/sparql-results+json', 'User-Agent': 'HSD_DAW_BOT/1.0 (https://github.com/Sin-Yone/movie-db-sparql) python-requests'})
-    print(r.text)
-    print(r.json()['results']['bindings'])
+                     headers={'accept': 'application/sparql-results+json',
+                              'User-Agent': 'HSD_DAW_BOT/1.0 (https://github.com/Sin-Yone/movie-db-sparql) python-requests'})
     results = r.json()['results']['bindings']
-    if not results: continue
+    sortedMovies = {}
+    for movieData in results:
+        if not movieData: continue
+        if movieData['id']['value'] not in sortedMovies:
+            sortedMovies[movieData['id']['value']] = [movieData]
+        else:
+            sortedMovies[movieData['id']['value']].append(movieData)
+    for movieId, movies in sortedMovies.items():
+        # merge all movie data into one object
+        newMovieData = {
+            'director': [],
+            'cast_member': [],
+            'voice_actor': [],
+            'producer': [],
+            'composer': [],
+            'main_subject': [],
+            'publication_date': movies[0].get('publicationDate', {'value': ''})['value'],
+            'cost': movies[0].get('cost', {'value': ''})['value'],
+            'follows': movies[0].get('followsLabel', {'value': ''})['value'],
+            'followed_by': movies[0].get('followedByLabel', {'value': ''})['value'],
+        }
 
-    # Zusammenfassen der Daten
-    newMovieData = {
-        'director': [],
-        'voice_actor': [],
-        'producer': [],
-        'composer': [],
-        'publication_date': results[0].get('publicationDate', {'value': ''})['value'],
-        'cost': results[0].get('cost', {'value': ''})['value']
-    }
+        for movie in movies:
+            if movie.get('directorLabel') and movie['directorLabel']['value'] not in newMovieData['director']:
+                newMovieData[
+                    'director'].append(movie['directorLabel']['value'])
+            if movie.get('castMemberLabel') and movie['castMemberLabel']['value'] not in newMovieData['cast_member']:
+                newMovieData[
+                    'cast_member'].append(movie['castMemberLabel']['value'])
+            if movie.get('voiceActorLabel') and movie['voiceActorLabel']['value'] not in newMovieData['voice_actor']:
+                newMovieData[
+                    'voice_actor'].append(movie['voiceActorLabel']['value'])
+            if movie.get('producerLabel') and movie['producerLabel']['value'] not in newMovieData['producer']:
+                newMovieData[
+                    'producer'].append(movie['producerLabel']['value'])
+            if movie.get('composerLabel') and movie['composerLabel']['value'] not in newMovieData['composer']:
+                newMovieData[
+                    'composer'].append(movie['composerLabel']['value'])
+            if movie.get('mainSubjectLabel') and movie['mainSubjectLabel']['value'] not in newMovieData['main_subject']:
+                newMovieData[
+                    'main_subject'].append(movie['mainSubjectLabel']['value'])
 
-    for result in results:
-        if result.get('directorLabel') and result['directorLabel']['value'] not in newMovieData['director']: newMovieData['director'].append(result['directorLabel']['value'])
-        if result.get('voiceActorLabel') and result['voiceActorLabel']['value'] not in newMovieData['voice_actor']: newMovieData['voice_actor'].append(result['voiceActorLabel']['value'])
-        if result.get('producerLabel') and result['producerLabel']['value'] not in newMovieData['producer']: newMovieData['producer'].append(result['producerLabel']['value'])
-        if result.get('composerLabel') and result['composerLabel']['value'] not in newMovieData['composer']: newMovieData['composer'].append(result['composerLabel']['value'])
-
-    print("New Data")
-    print(newMovieData)
-    # Importieren in DB
-    collection.find_one_and_update({'title': results[0]['filmLabel']['value']}, {'$set': newMovieData})
+        # Update db with new data
+        collection.update_many({'imdb_id': movieId}, {'$set': newMovieData})
+    fetchedElements += len(currentIds)
+    print('Movies updated: {0} ({1}%)'.format(fetchedElements, round((fetchedElements * 100) / len(ids), 2)))
